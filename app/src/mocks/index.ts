37e00a4bb2dead04fea68fb7e77e70b87e3ba095
gaowei1012 <gyb2020018@163.com>
2022-06-14 17:53:34 +0800

    删除测试项目 app

--- app/src/mocks/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: mingwei
- * @Date: 2022-06-14 10:13:40
- * @LastEditors: mingwei
- * @LastEditTime: 2022-06-14 10:13:41
- * @FilePath: /vite-project/src/mocks/index.ts
- * @Description:
- */
-export {};
