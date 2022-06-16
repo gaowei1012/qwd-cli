70f7681ce32fcc93165f26a0b8b38af83ef84f30
gaowei1012 <gyb2020018@163.com>
2022-06-16 16:39:09 +0800

    删除不需要代码

--- app/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-import { observer } from "mobx-react-lite";
-import { useStore } from "./hooks/useStore";
-
-const App = observer(() => {
-  return <div>app</div>;
-});
-
-export default App;
--- app/src/services/apis/login.ts
+++
@@ -1,4 +0,0 @@
-export const login = `login`;
-export const register = `register`;
-
-export const change_password = "auth/user/change_password";
