3c2ccf5238bb98c0d67643671ef46b0aeb3f3859
执念 <gyb2020018@163.com>
2022-06-17 13:43:20 +0800

    Revert "修复拷贝文件📃bug"

--- antd-form-block/demo.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: mingwei
- * @Date: 2022-06-17 10:46:42
- * @LastEditors: mingwei
- * @LastEditTime: 2022-06-17 10:46:42
- * @FilePath: /dg-cli/block/antd-form-block/demo.js
- * @Description:
- */
--- antd-modal-block/index.tsx
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: mingwei
- * @Date: 2022-06-17 10:28:38
- * @LastEditors: mingwei
- * @LastEditTime: 2022-06-17 10:28:38
- * @FilePath: /dg-cli/block/antd-modal-block/index.tsx
- * @Description:
- */
